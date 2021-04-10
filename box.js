5802db1f7bfa9d53bf86473911fd66d5e66ce0a1
pratham267 <81231649+pratham267@users.noreply.github.com>
2021-04-10 16:27:48 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,21 @@
+class box{
+    constructor(x,y,width,height){
+        var options={
+            'restitution':1.0
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos=this.body.position;
+        push();
+        translate(pos.x,pos.y);
+     
+        rectMode(CENTER);
+        fill(255,0,0);
+        rect(0,0,this.width,this.height);
+        pop();
+    }
+}
